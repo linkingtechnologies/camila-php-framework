3ef0aab9e7d93e1f575e167e9069bc9884aa69b4
umbi <umbi@linkingtechnologies.it>
2022-11-19 23:27:27 +0100

    PHP 8 compatibility

--- camila/datagrid/configurator.class.php
+++ camila/datagrid/configurator.class.php
@@ -510,12 +510,12 @@ class configurator
                 for ($j = 1; $j <= $data->rowcount($sheetnum); $j++) {
                     $curr = $data->type($j, $i, $sheetnum);
 					//echo $curr;
-					try {
+					//FIX ME
+					/*try {
 						if ($data->sheets[$sheetnum]['cellsInfo'][$j][$i]['hyperlink']['link'] != '')
 							$curr = 'hyperlink';
 					} catch (Exception $e) {
-						//Fix me
-					}
+					}*/
 					//PHPSpreadsheet						
 					$reflector = new ReflectionClass(get_class($data));
 					if ($this->ends_with($reflector->getFileName(),'excel_reader_wrapper.php')) {
